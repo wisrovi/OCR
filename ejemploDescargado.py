70c12e0e6db5e6bcfaba0cbcbeeb5f663bda5103
William Rodriguez <wisrovi.rodriguez@gmail.com>
2020-02-06 18:40:05 -0500

    Add files via upload

---
+++ ejemploDescargado.py
@@ -0,0 +1,141 @@
+import numpy as np
+import cv2
+import requests
+ 
+IMAGEFILE = 'captcha.png'
+CONTOURIMAGEFILE = 'contour.png'
+REQUESTURL='http://challenge01.root-me.org/programmation/ch8/'
+REPRSIZE = 20
+IMGSIZE = 500
+CENTER = (IMGSIZE/2, IMGSIZE/2)
+ 
+def createtrainingset(charset):
+    Xtrain=[]
+    ytrain=[]
+   
+    for char in charset:
+        for charthickness in range(2,4):
+            for angle in np.arange(-5, 5, 1):
+ 
+                charsize = 5
+                scale = 1.0
+ 
+                img=np.zeros((IMGSIZE, IMGSIZE))
+                #cv2.putText(img, char, CENTER, cv2.FONT_HERSHEY_PLAIN, charsize, 255, charthickness)
+ 
+                rotmat = cv2.getRotationMatrix2D(CENTER, angle, scale)
+                res = cv2.warpAffine(img, rotmat, img.shape)
+ 
+                res = res.astype('uint8')
+ 
+                contours, hierarchy = cv2.findContours(res,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+ 
+                for idx, contour in enumerate(contours):
+                    if hierarchy[0,idx,3]==-1:
+                       
+                        # straight bounding rectangle
+                        x,y,w,h = cv2.boundingRect(contour)
+ 
+                        # ignore dots (i and j)
+                        if h<30: continue
+ 
+                        # translate contour
+                        n = contour.shape[0]
+                        transcontour=contour-np.array([[[x, y]]]*n)
+ 
+                        # create normalized representation for alphanumeric character
+                        representation = np.zeros((h, w))
+                        cv2.fillPoly(representation, [transcontour], color=255)
+ 
+                        # resize alphanumeric character
+                        representation = cv2.resize(representation, (REPRSIZE, REPRSIZE), interpolation = cv2.INTER_LINEAR)
+ 
+                        # append representation to training set
+                        Xtrain.append(np.float32(representation.reshape(REPRSIZE*REPRSIZE)))
+                        ytrain.append(np.float32(ord(char)))
+ 
+    # transform into numpy arrays
+    return (np.array(Xtrain), np.array(ytrain))
+ 
+# create training set
+print( 'creating training set...')
+Xtrain, ytrain = createtrainingset('QWERTYUIOPASDFGHJKLZXCVBNM123456789qwertyuiopsadfghjklzxcvbnm')
+ 
+# create knn
+print( 'training knn...')
+knn = cv2.KNearest()
+knn.train(Xtrain, ytrain)
+ 
+# 100 trials
+for i in range(100):
+   
+    # request captcha
+    resp = requests.get(REQUESTURL)
+ 
+    # extract and save image
+    data=resp.content.split('base64,')[1].split('"')[0]
+    with open(IMAGEFILE, 'wb') as output:
+        output.write(data.decode('base64'))
+ 
+    # load image
+    img = cv2.imread(IMAGEFILE)
+ 
+    # convert to gray scale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+    # thresholding
+    neg = 255-gray
+    ret,thresh = cv2.threshold(neg, 245,-1,cv2.THRESH_TOZERO_INV)
+    ret,thresh = cv2.threshold(thresh,100,255,cv2.THRESH_BINARY)
+ 
+    # find contours
+    contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+ 
+    # remove contours that are not on top of the hierarchy
+    contours = [contour for idx, contour in enumerate(contours) if hierarchy[0, idx, 3]==-1]
+ 
+    # remove too small contours to ignore dots (i and j)
+    contours = [contour for contour in contours if cv2.boundingRect(contour)[3]>8]
+ 
+    res=[]
+    for idx, contour in enumerate(contours):
+        # straight bounding rectangle
+        x,y,w,h = cv2.boundingRect(contour)
+ 
+        # translate contour
+        n = contour.shape[0]
+        transcontour=contour-np.array([[[x, y]]]*n)
+ 
+        # create normalized representation for alphanumeric character
+        representation = np.zeros((h, w))
+        cv2.fillPoly(representation, [transcontour], color=255)
+ 
+        # resize alphanumeric character
+        representation = cv2.resize(representation, (REPRSIZE, REPRSIZE), interpolation = cv2.INTER_LINEAR)
+       
+        # write contour
+        tmp = img.copy()
+        cv2.drawContours(tmp, [contour], 0, (0,255,0), 1)
+        cv2.imwrite(CONTOURIMAGEFILE, tmp)
+ 
+        # inputsample for classifiers
+        inputsample = np.float32(representation.reshape(1,REPRSIZE*REPRSIZE))
+ 
+        # predict with knn
+        ret, results, neighbours, dist = knn.find_nearest(inputsample, 3)
+ 
+        # append prediction with x coordinate
+        res.append((chr(results[0, 0]), x))
+ 
+    # reorder letters
+    res=sorted(res, key=lambda x: x[1])
+    res="".join([x[0] for x in res])
+ 
+    print( 'trial', i+1, res)
+ 
+    # send payload
+    payload = {'cametu': res}
+    resp = requests.post(REQUESTURL, payload, cookies=resp.cookies)
+    if 'flag' in resp.content:
+        print(resp.content)
+        break
---
+++ pruebaCaptcha.py
@@ -0,0 +1,148 @@
+import urllib.request
+import urllib.parse
+import requests 
+import re
+import base64
+import subprocess
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+#borrar +++++++++++++++++++++
+#https://towardsdatascience.com/read-text-from-image-with-one-line-of-python-code-c22ede074cac
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+#+++++++++++++++++++
+
+
+def DescargarImagenCAPTCHA(url, nombreGuardarImagen):
+    #extraer la pagina HTML    
+    req = urllib.request.Request(urlServidorImagen)
+    response = urllib.request.urlopen(req)
+    html = response.read()
+    stringHtml = html.decode("utf-8")  #necesario convertirlo a string para poder buscar en el html (formato orginal: bytes)
+
+    #Buscar en el HTML la ruta para extraer sólo la imagen    
+    regex = r'data:image/png;base64,(.*)" /><br><br>'
+    result = re.search(regex, stringHtml)
+    result = result.group(1)
+
+    #Decodificar la url de la imagen y guardar la imagen    
+    result = base64.b64decode(result)  #tengo la imagen en una variable
+    file_handle = open(nombreGuardarImagen, 'wb')
+    file_handle.write(result)   #guardo la imagen
+    file_handle.close
+    
+    file_handle = open(nombreGuardarImagen, 'rb')
+    result = subprocess.Popen(['gocr -i captcha.png'], shell=True, stdout=subprocess.PIPE).communicate()[0]
+    file_handle.close
+
+def LimpiarImagen(nombreImagen):
+    img = cv2.imread(nameFile,0)
+    imagenFiltrada = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)[1]
+    imagenFiltrada = cv2.bitwise_not(imagenFiltrada)
+    return imagenFiltrada
+
+def ReforzarColores(imagen, nombreImagen):
+    cv2.imwrite(nombreImagen, imagenFiltrada)
+    imagen = cv2.imread(nameFileFilter)
+    hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
+    grises_bajos = np.array([232,232,232], dtype=np.uint8)
+    grises_altos = np.array([0, 0, 0], dtype=np.uint8)
+    mascara_negra = cv2.inRange(hsv, grises_altos, grises_bajos)
+    cv2.imwrite(nombreImagen, mascara_negra)
+    imagen = cv2.imread(nombreImagen)
+    imagen = cv2.bitwise_not(imagen)
+    cv2.imwrite(nombreImagen, imagen)
+    imagen = cv2.imread(nombreImagen)
+    return imagen
+
+
+
+
+
+
+
+print("Librerias cargadas")
+
+
+
+
+nameFile = 'captcha.png'
+nameFileFilter = 'captcha-filtrado.png'
+urlServidorImagen = "http://challenge01.root-me.org/programmation/ch8/"
+
+opener = urllib.request.build_opener()
+#opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+#opener.addheaders.append(('Connection', 'Keep-Alive'))
+
+
+opener.addheaders = [('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:23.0) Gecko/20100101 Firefox/23.0')]
+opener.addheaders.append(('Cookie', 'challenge_frame=1; spip_session=myspip_session; PHPSESSID=myPHPSESSID'))
+#opener.addheaders.append(('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'))
+opener.addheaders.append(('Accept-Language', 'en-US,en;q=0.5'))
+#opener.addheaders.append(('Accept-Encoding', 'gzip, deflate'))
+opener.addheaders.append(('DNT', '1'))
+opener.addheaders.append(('Connection', 'Keep-Alive'))
+
+
+response = opener.open(urlServidorImagen)
+html = response.read()
+stringHtml = html.decode("utf-8")
+regex = r'data:image/png;base64,(.*)" /><br><br>'
+result = re.search(regex, stringHtml)
+result = result.group(1)
+result = base64.b64decode(result)
+file_handle = open(nameFile, 'wb')
+file_handle.write(result)   #guardo la imagen
+file_handle.close
+file_handle = open(nameFile, 'rb')
+result = subprocess.Popen(['gocr -i captcha.png'], shell=True, stdout=subprocess.PIPE).communicate()[0]
+file_handle.close
+
+
+#DescargarImagenCAPTCHA(urlServidorImagen, nameFile)
+imagenFiltrada = LimpiarImagen(nameFile)
+#imagenReforzada = ReforzarColores(imagenFiltrada, nameFileFilter)
+
+
+result = pytesseract.image_to_string(imagenFiltrada) 
+print( result )
+
+
+values = {'cametu':result}
+post_data = urllib.parse.urlencode(values)
+post_data = post_data.encode('ascii')
+
+#opener.addheaders.append(('Referer', urlServidorImagen))
+response = opener.open(urlServidorImagen, post_data)
+
+while True:
+    html = response.read()
+    stringHtml = html.decode("utf-8")
+    if not('Failed' in stringHtml) and stringHtml:
+        break
+
+#file_handle = open('result.html', 'w')
+#file_handle.write(html.decode("utf-8"))
+#file_handle.close
+
+print(html)
+
+
+plt.imshow(imagenFiltrada,'gray')
+plt.show()
+
+
+""" 
+
+imagenReforzada = cv2.imread(nameFileFilter)
+
+result = pytesseract.image_to_string(imagenFiltrada) 
+values = {'cametu':result}
+print( result )
+
+r = requests.post(url = urlServidorImagen, data = values) 
+pastebin_url = r.text
+print("The pastebin URL is:%s"%pastebin_url)
+"""
